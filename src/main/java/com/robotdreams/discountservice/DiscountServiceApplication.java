409b8c09fe97d6eeb6ebd1da8d047649e275dfc1
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2024-05-18 13:16:53 +0300

    print jwt for test purposes

--- src/main/java/com/robotdreams/discountservice/DiscountServiceApplication.java
+++ src/main/java/com/robotdreams/discountservice/DiscountServiceApplication.java
@@ -1,7 +1,11 @@
 package com.robotdreams.discountservice;
 
+import com.robotdreams.discountservice.util.JwtUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class DiscountServiceApplication {
@@ -9,6 +13,11 @@ public class DiscountServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiscountServiceApplication.class, args);
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("ROLES", "ROLE_ADMIN");
+
+        JwtUtil.generateToken(claims, "denemeName");
 
     }
 
